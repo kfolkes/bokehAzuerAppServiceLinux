65f151af98c29e6603c03e3ec714579212c33b35
Krystal Folkes <kfolkes@users.noreply.github.com>
2019-10-10 16:57:54 -0500

    start up script

---
+++ bokehstart.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Enter the source directory to make sure the script runs where the user expects+
+
+cd /home/site/wwwroot
+pip install -r requirements.txt
+export PORT=8000
+bokeh serve app.py --allow-websocket-origin "*" --port 8000 --use-xheaders --address 0.0.0.0 
+
+
