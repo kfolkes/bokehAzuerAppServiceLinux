cb93e18e7e49e9ee39d5e43b263259dd4fb2f5a3
Krystal Folkes <kfolkes@users.noreply.github.com>
2019-10-16 11:50:19 -0500

    removed pip install

--- bokehstart.sh
+++ bokehstart.sh
@@ -2,7 +2,7 @@
 # Enter the source directory to make sure the script runs where the user expects+
 
 cd /home/site/wwwroot
-pip install -r requirements.txt
+#pip install -r requirements.txt
 export PORT=8000
 bokeh serve appbok.py --allow-websocket-origin "*" --port 8000 --use-xheaders --address 0.0.0.0 
 
